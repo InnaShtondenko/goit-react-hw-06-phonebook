40e727c0073df50daf3fe69d9c7882ee922c199d
InnaShtondenko <103380613+InnaShtondenko@users.noreply.github.com>
2022-12-19 15:46:20 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,31 @@
+import { PropTypes } from 'prop-types';
+import { ContactListItem } from './ContactListItem/ContactListItem';
+import { ContactsList } from './ContactList.styled';
+
+
+export function ContactList(props) {
+  const { contacts, onContactRemoveCallback } = props;
+
+  return (
+    <ContactsList>
+      {contacts.map(contactData => (
+        <ContactListItem
+          key={contactData.id}
+          contactData={contactData}
+          onContactRemoveCallback={onContactRemoveCallback}
+        />
+      ))}
+    </ContactsList>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ),
+  onContactRemoveCallback: PropTypes.func.isRequired,
+};
