ad8395fe15ebf41ef7e5fadd13df6a0b673c153d
InnaShtondenko <inna.mycase@gmail.com>
2023-01-08 13:47:25 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,31 +1,34 @@
-import { PropTypes } from 'prop-types';
+import { useSelector } from 'react-redux';
+
+import { getContacts, getFilter } from 'redux/selectors';
+
 import { ContactListItem } from './ContactListItem/ContactListItem';
 import { ContactsList } from './ContactList.styled';
 
 
-export function ContactList(props) {
-  const { contacts, onContactRemoveCallback } = props;
+export function ContactList(){
+  const filterValue = useSelector(getFilter);
+  const contactsData = useSelector(getContacts);
+
+  const filteredContacts = (() => {
+    const normalizedFilter = filterValue.toLowerCase().trim();
+
+    if (!normalizedFilter) {
+      return null;
+    }
+    return contactsData.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  })();
 
   return (
     <ContactsList>
-      {contacts.map(contactData => (
+      {(filteredContacts ?? contactsData).map(contactData => (
         <ContactListItem
           key={contactData.id}
           contactData={contactData}
-          onContactRemoveCallback={onContactRemoveCallback}
         />
       ))}
     </ContactsList>
   );
 }
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-      id: PropTypes.string.isRequired,
-    })
-  ),
-  onContactRemoveCallback: PropTypes.func.isRequired,
-};
